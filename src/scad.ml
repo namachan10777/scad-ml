9333dd31975a33a1e7d00d190b1b768b99fb5a9c
namachan10777 <namachan10777@gmail.com>
2019-01-26 18:43:17 +0900

    impl pretty printer

--- src/scad.ml
+++ src/scad.ml
@@ -10,14 +10,31 @@ type scad_t =
     | Rotate of rotate_t * scad_t
     | Union of scad_t list
     | Intersection of scad_t list
-    | Difference of scad_t list * scad_t
+    | Difference of scad_t * scad_t list
 
-let string_of_scad = function
-    | Cylinder { r1; r2; h; center } ->
-        Printf.sprintf "cylinder(h=%f, r1=%f, r2=%f, center=%B)" h r1 r2 center
-    | Cube { size; center } ->
-        let (w, h, d) = size in
-        Printf.sprintf "cube(size=[%f, %f, %f], center=%B)" w h d center
-    | Sphere { r } ->
-        Printf.sprintf "sphere(%f)" r
-    | _ -> ""
+let string_of_pos_t = function (w, h, d) -> Printf.sprintf "[%f, %f, %f]" w h d
+let string_of_rotate_t =
+    let deg_of_rad r = 360.0 *. r /. (2. *. pi) in
+    function (w, h, d) -> Printf.sprintf "[%f, %f, %f]" (deg_of_rad w) (deg_of_rad h) (deg_of_rad d)
+
+let string_of_scad =
+    let rec arrange_elms indent = List.fold_left (fun stmts scad -> stmts^(print indent scad)) ""
+    and print indent = function
+        | Cylinder { r1; r2; h; center } ->
+                Printf.sprintf "%scylinder(h=%f, r1=%f, r2=%f, center=%B)\n" indent h r1 r2 center
+        | Cube { size; center } ->
+            let (w, h, d) = size in
+            Printf.sprintf "%scube(size=[%f, %f, %f], center=%B)\n" indent w h d center
+        | Sphere { r } ->
+            Printf.sprintf "%ssphere(%f)\n" indent r
+        | Translate (p, scad) ->
+            Printf.sprintf "%stranslate(%s)\n%s" indent (string_of_pos_t p) (print (indent^"\t") scad)
+        | Rotate (r, scad) ->
+            Printf.sprintf "%srotate(%s)\n%s" indent (string_of_rotate_t r) (print (indent^"\t") scad)
+        | Union elements ->
+            Printf.sprintf "%sunion(){\n%s}\n" indent @@ arrange_elms (indent^"\t") elements
+        | Intersection elements ->
+            Printf.sprintf "%sintersection(){\n%s}\n" indent @@ arrange_elms (indent^"\t") elements
+        | Difference (minuend, subtrahend) ->
+            Printf.sprintf "%sdifference(){\n%s%s}\n" indent (print (indent^"\t") minuend) @@ arrange_elms (indent^"\t") subtrahend
+    in print ""
