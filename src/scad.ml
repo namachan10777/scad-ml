01cade7230865bbe943833b24a2c5e69dd0ecf0d
namachan10777 <namachan10777@gmail.com>
2019-01-27 15:13:15 +0900

    add Vector operation helper

--- src/scad.ml
+++ src/scad.ml
@@ -84,3 +84,15 @@ module Scad = struct
         Printf.fprintf oc "%s" (Scad_core.string_of_scad scad);
         flush oc
 end
+
+module Math = struct
+    module Pos = struct
+        let horizontal_op op p1 p2 = match p1, p2 with
+            (x1, y1, z1), (x2, y2, z2) -> (op x1 x2), (op y1 y2), (op z1 z2)
+
+        let add = horizontal_op (+.)
+        let sub = horizontal_op (-.)
+        let mul = horizontal_op ( *.)
+        let div = horizontal_op (/.)
+    end
+end
