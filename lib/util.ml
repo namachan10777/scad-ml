f1b5e48e63390700027191f487f683d83dc48c01
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-19 00:56:23 -0700

    Add rotate_about_pt and negate utilities

--- lib/util.ml
+++ lib/util.ml
@@ -1,14 +1,14 @@
-let get_x = function (x, _, _) -> x
-let get_y = function (_, y, _) -> y
-let get_z = function (_, _, z) -> z
-
+let get_x (x, _, _) = x
+let get_y (_, y, _) = y
+let get_z (_, _, z) = z
 let ( |>> ) src p = Model.translate p src
 let ( |@> ) src r = Model.rotate r src
 let ( <+> ) = Math.add
 let ( <-> ) = Math.sub
 let ( <*> ) = Math.mul
 let ( </> ) = Math.div
+let rotate_about_pt r p scad = scad |>> p |@> r |>> Math.negate p
 
 let write oc scad =
-    Printf.fprintf oc "%s" (Core.string_of_scad scad);
-    flush oc
+  Printf.fprintf oc "%s" (Core.string_of_scad scad);
+  flush oc
