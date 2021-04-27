5678eae32f70dc789e36612eab988f0376516d49
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-04-26 22:18:46 -0700

    Add point rotation functions

--- lib/math.ml
+++ lib/math.ml
@@ -11,3 +11,26 @@ let sub = horizontal_op ( -. )
 let mul = horizontal_op ( *. )
 let div = horizontal_op ( /. )
 let negate = horizontal_op ( *. ) (-1., -1., -1.)
+
+let rotate_x theta (x, y, z) =
+  let s = Float.sin theta in
+  let c = Float.cos theta in
+  let y' = (y *. c) -. (z *. s) in
+  let z' = (z *. c) +. (y *. s) in
+  x, y', z'
+
+let rotate_y theta (x, y, z) =
+  let s = Float.sin theta in
+  let c = Float.cos theta in
+  let x' = (x *. c) +. (z *. s) in
+  let z' = (z *. c) -. (x *. s) in
+  x', y, z'
+
+let rotate_z theta (x, y, z) =
+  let s = Float.sin theta in
+  let c = Float.cos theta in
+  let x' = (x *. c) -. (y *. s) in
+  let y' = (y *. c) +. (x *. s) in
+  x', y', z
+
+let rotate (tx, ty, tz) p = rotate_x tx p |> rotate_y ty |> rotate_z tz
