e91dd2a4d3758e09a572e80e69389f0b078acc9d
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-06-09 00:45:35 -0700

    Cross product

--- lib/math.ml
+++ lib/math.ml
@@ -42,3 +42,6 @@ let norm ((x, y, z) as p) =
   if mag > 0. then x /. mag, y /. mag, z /. mag else p
 
 let dot (x1, y1, z1) (x2, y2, z2) = (x1 *. x2) +. (y1 *. y2) +. (z1 *. z2)
+
+let cross (x1, y1, z1) (x2, y2, z2) =
+  (y1 *. z2) -. (z1 *. y2), (z1 *. x2) -. (x1 *. z2), (x1 *. y2) -. (y1 *. x2)
