e769e916b070870c656a03df15a5b4b4396256af
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-06-13 14:14:05 -0700

    Quaternion to MultMatrix completed

---
+++ lib/quaternion.mli
@@ -0,0 +1,19 @@
+type t = float * float * float * float
+
+val id : t
+val make : float * float * float -> float -> t
+val add : t -> t -> t
+val sub : t -> t -> t
+val add_scalar : t -> float -> t
+val sub_scalar : t -> float -> t
+val scalar_sub_quat : t -> float -> t
+val mul : t -> t -> t
+val mul_scalar : t -> float -> t
+val div_scalar : t -> float -> t
+val negate : t -> t
+val norm : t -> float
+val normalize : t -> t
+val dot : t -> t -> float
+val conj : t -> t
+val distance : t -> t -> float
+val to_multmatrix : t -> MultMatrix.t
