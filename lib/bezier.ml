9aa3be98bd02e625216ab7c34249034358cd3f0b
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-01-15 22:51:17 -0800

    Throw exception on illegal t values

--- lib/bezier.ml
+++ lib/bezier.ml
@@ -1,10 +1,12 @@
 let quad_weights t =
+  if t < 0. || t > 1. then failwith "Bézier function undefined outside of 0 -> 1.";
   let a = (1. -. t) ** 2.
   and b = 2. *. t *. (1. -. t)
   and c = t ** 2. in
   a, b, c
 
 let cubic_weights t =
+  if t < 0. || t > 1. then failwith "Bézier function undefined outside of 0 -> 1.";
   let a = (1. -. t) ** 3.
   and b = 3. *. t *. ((1. -. t) ** 2.)
   and c = 3. *. (t ** 2.) *. (1. -. t)
@@ -45,9 +47,7 @@ let cubic_vec3 ~p1:(x1, y1, z1) ~p2:(x2, y2, z2) ~p3:(x3, y3, z3) ~p4:(x4, y4, z
   and z = (a *. z1) +. (b *. z2) +. (c *. z3) +. (d *. z4) in
   x, y, z
 
-let curve ?(init = []) ?(rev = false) ~n_steps bez =
-  let dt = 1. /. Float.of_int n_steps *. if rev then 1. else -1. in
-  let rec loop acc i t =
-    if i <= n_steps then loop (bez t :: acc) (i + 1) (t +. dt) else acc
-  in
+let curve ?(init = []) ?(rev = false) ?(fn = 10) bez =
+  let dt = 1. /. Float.of_int fn *. if rev then 1. else -1. in
+  let rec loop acc i t = if i <= fn then loop (bez t :: acc) (i + 1) (t +. dt) else acc in
   loop init 0 (if rev then 0. else 1.)
--- lib/bezier.mli
+++ lib/bezier.mli
@@ -1,35 +1,41 @@
 (** [quad ~p1 ~p2 ~p3 t]
 
-    Quadratic Bézier float interpolation. *)
+    Quadratic Bézier float interpolation. Throws exception when [t] is outside
+    the range of 0 -> 1. *)
 val quad : p1:float -> p2:float -> p3:float -> float -> float
 
 (** [cubic ~p1 ~p2 ~p3 ~p4 t]
 
-    Cubic Bézier float interpolation. *)
+    Cubic Bézier float interpolation. Throws exception when [t] is outside
+    the range of 0 -> 1. *)
 val cubic : p1:float -> p2:float -> p3:float -> p4:float -> float -> float
 
 (** [quad_vec2 ~p1 ~p2 ~p3 t]
 
     Quadratic Bézier function, interpolating a path in 2d space starting from
-    [p1] when [t = 0.] and ending at [p3] when [t = 1.]. *)
+    [p1] when [t = 0.] and ending at [p3] when [t = 1.]. Throws exception when
+    [t] is outside the range of 0 -> 1*)
 val quad_vec2 : p1:Vec2.t -> p2:Vec2.t -> p3:Vec2.t -> float -> Vec2.t
 
 (** [cubic_vec2 ~p1 ~p2 ~p3 ~p4 t]
 
-    Cubic Bézier function, interpolating a path in 2d space starting from
-    [p1] when [t = 0.] and ending at [p4] when [t = 1.]. *)
+    Cubic Bézier function, interpolating a path in 2d space starting from [p1]
+    when [t = 0.] and ending at [p4] when [t = 1.]. Throws exception when [t] is
+    outside the range of 0 -> 1*)
 val cubic_vec2 : p1:Vec2.t -> p2:Vec2.t -> p3:Vec2.t -> p4:Vec2.t -> float -> Vec2.t
 
 (** [quad_vec3 ~p1 ~p2 ~p3 t]
 
     Quadratic Bézier function, interpolating a path in 3d space starting from
-    [p1] when [t = 0.] and ending at [p3] when [t = 1.]. *)
+    [p1] when [t = 0.] and ending at [p3] when [t = 1.]. Throws exception when
+    [t] is outside the range of 0 -> 1*)
 val quad_vec3 : p1:Vec3.t -> p2:Vec3.t -> p3:Vec3.t -> float -> Vec3.t
 
 (** [cubic_vec3 ~p1 ~p2 ~p3 ~p4 t]
 
-    Cubic Bézier function, interpolating a path in 3d space starting from
-    [p1] when [t = 0.] and ending at [p4] when [t = 1.]. *)
+    Cubic Bézier function, interpolating a path in 3d space starting from [p1]
+    when [t = 0.] and ending at [p4] when [t = 1.]. Throws exception when [t] is
+    outside the range of 0 -> 1*)
 val cubic_vec3 : p1:Vec3.t -> p2:Vec3.t -> p3:Vec3.t -> p4:Vec3.t -> float -> Vec3.t
 
 (** [curve ?init ?rev ~n_steps bez t]
@@ -39,4 +45,4 @@ val cubic_vec3 : p1:Vec3.t -> p2:Vec3.t -> p3:Vec3.t -> p4:Vec3.t -> float -> Ve
     to [init] if provided. Reverse order can be useful for building up a
     path/polygon with mixed prepending of points calculated by other means
     and/or multiple uses of {!curve}. *)
-val curve : ?init:'a list -> ?rev:bool -> n_steps:int -> (float -> 'a) -> 'a list
+val curve : ?init:'a list -> ?rev:bool -> ?fn:int -> (float -> 'a) -> 'a list
