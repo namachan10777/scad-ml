bd83c90c0f1c5be1da87d8a224f2a054f19b9e21
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-01-26 00:56:41 -0800

    Add resample, + extend to_transforms (scale/twist)

--- lib/path.ml
+++ lib/path.ml
@@ -1,9 +1,75 @@
-let to_transforms path =
+let total_travel = function
+  | [] | [ _ ] -> 0.
+  | hd :: tl   ->
+    let f (sum, last) p = sum +. Vec3.distance p last, p in
+    fst @@ List.fold_left f (0., hd) tl
+
+let cummulative_travel = function
+  | []       -> []
+  | hd :: tl ->
+    let f (acc, sum, last) p =
+      let sum = sum +. Vec3.distance p last in
+      sum :: acc, sum, p
+    in
+    let travels, _, _ = List.fold_left f ([ 0. ], 0., hd) tl in
+    List.rev travels
+
+let to_continuous path =
+  let travels = Array.of_list (cummulative_travel path)
+  and path = Array.of_list path in
+  let len = Array.length travels in
+  let total = travels.(len - 1) in
+  let extrapolate s =
+    let d = Float.(min (max s 0.) 1.) *. total in
+    let i = ref 0
+    and p = ref None in
+    while Option.is_none !p && !i < len - 1 do
+      let idx = !i in
+      let d0 = Array.unsafe_get travels idx
+      and d1 = Array.unsafe_get travels (idx + 1) in
+      if d >= d0 && d <= d1
+      then (
+        let frac = (d -. d0) /. (d1 -. d0)
+        and p0 = Array.unsafe_get path idx
+        and p1 = Array.unsafe_get path (idx + 1) in
+        p := Some Vec3.(mul_scalar (p1 <-> p0) frac <+> p0) )
+      else incr i
+    done;
+    Option.get !p
+  in
+  extrapolate
+
+let of_continuous = Bezier.curve
+
+let resample ~freq path =
+  let n =
+    match freq with
+    | `N n       -> n
+    | `Spacing s -> Int.of_float @@ (total_travel path /. s)
+  in
+  let step = 1. /. Float.of_int (n - 1)
+  and f = to_continuous path in
+  List.init n (fun i -> f @@ (Float.of_int i *. step))
+
+let to_transforms ?scale ?twist path =
   let p = Array.of_list path in
   let len = Array.length p in
+  let scale =
+    match scale with
+    | Some (x, y) ->
+      let step = Vec3.map (fun a -> (a -. 1.) /. Float.of_int len) (x, y, 1.) in
+      fun i -> MultMatrix.scaling @@ Vec3.map (fun a -> (a *. Float.of_int i) +. 1.) step
+    | None        -> fun _ -> MultMatrix.id
+  and twist =
+    match twist with
+    | Some r ->
+      let step = r /. Float.of_int len in
+      fun i -> Quaternion.(to_multmatrix @@ make (0., 0., 1.) (step *. Float.of_int i))
+    | None   -> fun _ -> MultMatrix.id
+  in
   let f i =
-    let open Vec3 in
     let tangent =
+      let open Vec3 in
       ( if i = 0
       then p.(1) <-> p.(0)
       else if i = len - 1
@@ -11,6 +77,9 @@ let to_transforms path =
       else p.(i + 1) <-> p.(i - 1) )
       |> normalize
     in
-    Quaternion.(to_multmatrix ~trans:p.(i) @@ alignment (0., 0., 1.) tangent)
+    scale i
+    |> MultMatrix.mul (twist i)
+    |> MultMatrix.mul
+         Quaternion.(to_multmatrix ~trans:p.(i) @@ alignment (0., 0., 1.) tangent)
   in
   List.init len f
--- lib/path.mli
+++ lib/path.mli
@@ -1,3 +1,16 @@
+val total_travel : Vec3.t list -> float
+val cummulative_travel : Vec3.t list -> float list
+val to_continuous : Vec3.t list -> float -> Vec3.t
+
+val of_continuous
+  :  ?init:Vec3.t list
+  -> ?rev:bool
+  -> ?fn:int
+  -> (float -> Vec3.t)
+  -> Vec3.t list
+
+val resample : freq:[< `N of int | `Spacing of float ] -> Vec3.t list -> Vec3.t list
+
 (** [to_transforms t]
 
     Generate list of transformations that can be applied to three-dimensional
@@ -5,4 +18,4 @@
     {!Scad.multmatrix}), to move them along the [path] (intended to be applied to
     the vector/shape from its original position each time). Tangents are used to
     calculate appropriate rotations for each translation. *)
-val to_transforms : Vec3.t list -> MultMatrix.t list
+val to_transforms : ?scale:Vec2.t -> ?twist:float -> Vec3.t list -> MultMatrix.t list
