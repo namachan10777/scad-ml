c8dd238b033064956984ce3bab4d2f995bd3b456
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-01-27 01:02:11 -0800

    + helix and euler angle variation of to_transforms

--- lib/path.ml
+++ lib/path.ml
@@ -1,3 +1,5 @@
+type t = Vec3.t list
+
 let total_travel = function
   | [] | [ _ ] -> 0.
   | hd :: tl   ->
@@ -51,35 +53,56 @@ let resample ~freq path =
   and f = to_continuous path in
   List.init n (fun i -> f @@ (Float.of_int i *. step))
 
-let to_transforms ?scale ?twist path =
-  let p = Array.of_list path in
-  let len = Array.length p in
-  let scale =
-    match scale with
-    | Some (x, y) ->
-      let step = Vec3.map (fun a -> (a -. 1.) /. Float.of_int len) (x, y, 1.) in
-      fun i -> MultMatrix.scaling @@ Vec3.map (fun a -> (a *. Float.of_int i) +. 1.) step
-    | None        -> fun _ -> MultMatrix.id
-  and twist =
-    match twist with
-    | Some r ->
-      let step = r /. Float.of_int len in
-      fun i -> Quaternion.(to_multmatrix @@ make (0., 0., 1.) (step *. Float.of_int i))
-    | None   -> fun _ -> MultMatrix.id
-  in
+let helix ?fn ?fa ?fs ?(left = true) ~n_turns ~pitch ?r2 r1 =
+  let r2 = Option.value ~default:r1 r2 in
+  let n_frags = Util.helical_fragments ?fn ?fa ?fs (Float.max r1 r2) in
+  let r_step = (r2 -. r1) /. Float.of_int (n_turns * n_frags)
+  and h_step = pitch /. Float.of_int n_frags
+  and a_step = 2. *. Float.pi /. Float.of_int n_frags *. if left then -1. else 1. in
   let f i =
-    let tangent =
-      let open Vec3 in
-      ( if i = 0
-      then p.(1) <-> p.(0)
-      else if i = len - 1
-      then p.(i) <-> p.(i - 1)
-      else p.(i + 1) <-> p.(i - 1) )
-      |> normalize
-    in
-    scale i
-    |> MultMatrix.mul (twist i)
-    |> MultMatrix.mul
-         Quaternion.(to_multmatrix ~trans:p.(i) @@ alignment (0., 0., 1.) tangent)
+    let i = Float.of_int i in
+    let r = r1 +. (r_step *. i)
+    and a = a_step *. i in
+    Float.(r *. cos a, r *. sin a, h_step *. i)
   in
+  List.init ((n_frags * n_turns) + 1) f
+
+let scaler ~len (x, y) =
+  let step = Vec3.map (fun a -> (a -. 1.) /. Float.of_int len) (x, y, 1.) in
+  fun i -> MultMatrix.scaling @@ Vec3.map (fun a -> (a *. Float.of_int i) +. 1.) step
+
+let twister ~len r =
+  let step = r /. Float.of_int len in
+  fun i -> Quaternion.(to_multmatrix @@ make (0., 0., 1.) (step *. Float.of_int i))
+
+let to_transforms ?(euler = false) ?scale ?twist path =
+  let p = Array.of_list path in
+  let len = Array.length p
+  and id _ = MultMatrix.id in
+  let scale = Util.value_map_opt ~default:id (scaler ~len) scale
+  and twist = Util.value_map_opt ~default:id (twister ~len) twist
+  and transform =
+    let diff i =
+      let sub a b = Vec3.(p.(a) <-> p.(b)) in
+      if i = 0 then sub 1 0 else if i = len - 1 then sub i (i - 1) else sub (i + 1) (i - 1)
+    in
+    if euler
+    then (
+      let adjust =
+        let a = Float.pi /. 2. in
+        Quaternion.(to_multmatrix @@ of_euler (a, 0., a))
+      in
+      fun i ->
+        let dx, dy, dz = diff i in
+        let ay = Float.atan2 dz (Float.sqrt ((dx *. dx) +. (dy *. dy)))
+        and az = Float.atan2 dy dx in
+        MultMatrix.mul
+          Quaternion.(to_multmatrix ~trans:p.(i) (of_euler (0., ay, az)))
+          adjust )
+    else
+      fun i ->
+      let tangent = Vec3.normalize (diff i) in
+      Quaternion.(to_multmatrix ~trans:p.(i) @@ alignment (0., 0., 1.) tangent)
+  in
+  let f i = scale i |> MultMatrix.mul (twist i) |> MultMatrix.mul (transform i) in
   List.init len f
--- lib/path.mli
+++ lib/path.mli
@@ -1,21 +1,35 @@
-val total_travel : Vec3.t list -> float
-val cummulative_travel : Vec3.t list -> float list
-val to_continuous : Vec3.t list -> float -> Vec3.t
+type t = Vec3.t list
 
-val of_continuous
-  :  ?init:Vec3.t list
-  -> ?rev:bool
-  -> ?fn:int
-  -> (float -> Vec3.t)
-  -> Vec3.t list
+val total_travel : t -> float
+val cummulative_travel : t -> float list
+val to_continuous : t -> float -> Vec3.t
+val of_continuous : ?init:t -> ?rev:bool -> ?fn:int -> (float -> Vec3.t) -> t
+val resample : freq:[< `N of int | `Spacing of float ] -> t -> t
 
-val resample : freq:[< `N of int | `Spacing of float ] -> Vec3.t list -> Vec3.t list
+val helix
+  :  ?fn:int
+  -> ?fa:float
+  -> ?fs:float (* -> ?up:bool *)
+  -> ?left:bool
+  -> n_turns:int
+  -> pitch:float
+  -> ?r2:float
+  -> float
+  -> t
+
+val scaler : len:int -> float * float -> int -> MultMatrix.t
+val twister : len:int -> float -> int -> MultMatrix.t
 
 (** [to_transforms t]
 
     Generate list of transformations that can be applied to three-dimensional
-    vectors ({!Vec3.t} via {!MultMatrix.transform}) or shapes ({!Scad.d3} via
-    {!Scad.multmatrix}), to move them along the [path] (intended to be applied to
-    the vector/shape from its original position each time). Tangents are used to
-    calculate appropriate rotations for each translation. *)
-val to_transforms : ?scale:Vec2.t -> ?twist:float -> Vec3.t list -> MultMatrix.t list
+   vectors ({!Vec3.t} via {!MultMatrix.transform}) or shapes ({!Scad.d3} via
+   {!Scad.multmatrix}), to move them along the path [t] (intended to be applied
+   to the vector/shape from its original position each time). Tangents are used
+   to estimate appropriate rotations for each translation, using quaternion
+   alignment (from starting normal (0., 0. ,1.)) to the tangent vector by
+   default, when [euler = false]. Setting [euler = true] will use euler
+   rotations instead, which can have more desirable results in some scenarios,
+   but fail in others. For instance, [euler] can generate an abrupt when the
+   path tangent is exactly vertical. *)
+val to_transforms : ?euler:bool -> ?scale:Vec2.t -> ?twist:float -> t -> MultMatrix.t list
