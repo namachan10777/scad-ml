5bc8af10329f2ce5a0730ed962f1953e615e36ca
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-02-20 22:57:56 -0800

    Convert t to record for unboxing

--- lib/plane.ml
+++ lib/plane.ml
@@ -1,41 +1,46 @@
-type t = float * float * float * float
+type t =
+  { a : float
+  ; b : float
+  ; c : float
+  ; d : float
+  }
 
-let coefficients t = t
+let coefficients { a; b; c; d } = a, b, c, d
 
 let make p1 p2 p3 =
   let ((a, b, c) as crx) = Vec3.(cross (sub p3 p1) (sub p2 p1)) in
   let n = Vec3.norm crx in
   if Math.approx 0. n then invalid_arg "Plane points must not be colinear";
-  a /. n, b /. n, c /. n, Vec3.dot crx p1 /. n
+  { a = a /. n; b = b /. n; c = c /. n; d = Vec3.dot crx p1 /. n }
 
 let of_normal ?(point = Vec3.zero) ((x, y, z) as normal) =
   let n = Vec3.norm normal in
   if Math.approx 0. n then invalid_arg "Normal cannot be zero.";
-  x /. n, y /. n, z /. n, Vec3.dot normal point /. n
+  { a = x /. n; b = y /. n; c = z /. n; d = Vec3.dot normal point /. n }
 
-let project (a, b, c, d) =
+let project { a; b; c; d } =
   let n = a, b, c in
   let cp = Vec3.(div_scalar (mul_scalar n d) (dot n n)) in
   let rot = Quaternion.(to_multmatrix @@ alignment n (0., 0., 1.)) in
   let m = MultMatrix.(mul rot (translation (Vec3.negate cp))) in
   fun p -> Vec3.to_vec2 @@ MultMatrix.transform m p
 
-let lift (a, b, c, d) =
+let lift { a; b; c; d } =
   let n = a, b, c in
   let cp = Vec3.(div_scalar (mul_scalar n d) (dot n n)) in
   let rot = Quaternion.(to_multmatrix @@ alignment (0., 0., 1.) n) in
   let m = MultMatrix.(mul (translation cp) rot) in
   fun p -> MultMatrix.transform m (Vec3.of_vec2 p)
 
-let offset (a, b, c, d) = d /. Vec3.norm (a, b, c)
+let offset { a; b; c; d } = d /. Vec3.norm (a, b, c)
 
-let normalize (a, b, c, d) =
+let normalize { a; b; c; d } =
   let n = Vec3.norm (a, b, c) in
-  a /. n, b /. n, c /. n, d /. n
+  { a = a /. n; b = b /. n; c = c /. n; d = d /. n }
 
-let distance_to_point (a, b, c, d) p = Vec3.dot (a, b, c) p -. d
+let distance_to_point { a; b; c; d } p = Vec3.dot (a, b, c) p -. d
 
-let greatest_distance (a, b, c, d) ps =
+let greatest_distance { a; b; c; d } ps =
   let normal = a, b, c in
   let f (min, max) p =
     let n = Vec3.dot p normal in
@@ -46,4 +51,4 @@ let greatest_distance (a, b, c, d) ps =
 
 let are_points_on ?(eps = Util.epsilon) t ps = greatest_distance t ps < eps
 let is_point_above t p = distance_to_point t p > Util.epsilon
-let to_string (a, b, c, d) = Printf.sprintf "[%f, %f, %f, %f]" a b c d
+let to_string { a; b; c; d } = Printf.sprintf "[%f, %f, %f, %f]" a b c d
--- lib/quaternion.ml
+++ lib/quaternion.ml
@@ -1,40 +1,43 @@
-type t = float * float * float * float
+type t =
+  { x : float
+  ; y : float
+  ; z : float
+  ; w : float
+  }
 
-let id = 0., 0., 0., 1.
-let coefficients t = t
+let id = { x = 0.; y = 0.; z = 0.; w = 0. }
+let coefficients { x; y; z; w } = x, y, z, w
 
 let make ax angle =
   let x, y, z = Vec3.normalize ax in
   let s = Float.sin (angle /. 2.) in
-  x *. s, y *. s, z *. s, Float.cos (angle /. 2.)
+  { x = x *. s; y = y *. s; z = z *. s; w = Float.cos (angle /. 2.) }
 
-let basic_op op (x1, y1, z1, w1) (x2, y2, z2, w2) = op x1 x2, op y1 y2, op z1 z2, op w1 w2
+let basic_op op a b = { x = op a.x b.x; y = op a.y b.y; z = op a.z b.z; w = op a.w b.w }
 let add = basic_op ( +. )
 let sub = basic_op ( -. )
-let add_scalar (x, y, z, w) s = x, y, z, w +. s
-let sub_scalar (x, y, z, w) s = x, y, z, w -. s
-let scalar_sub_quat (x, y, z, w) s = -.x, -.y, -.z, s -. w
+let add_scalar t s = { t with w = t.w +. s }
+let sub_scalar t s = { t with w = t.w -. s }
+let scalar_sub_quat t s = { x = -.t.x; y = -.t.y; z = -.t.z; w = s -. t.w }
 
-let mul (x1, y1, z1, w1) (x2, y2, z2, w2) =
-  let x = (y1 *. z2) -. (z1 *. y2) +. (w2 *. x1) +. (w1 *. x2)
-  and y = (z1 *. x2) -. (x1 *. z2) +. (w2 *. y1) +. (w1 *. y2)
-  and z = (x1 *. y2) -. (y1 *. x2) +. (w2 *. z1) +. (z2 *. w1)
-  and w = (w1 *. w2) -. (x1 *. x2) -. (y1 *. y2) -. (z1 *. z2) in
-  x, y, z, w
+let mul a b =
+  let x = (a.y *. b.z) -. (a.z *. b.y) +. (b.w *. a.x) +. (a.w *. b.x)
+  and y = (a.z *. b.x) -. (a.x *. b.z) +. (b.w *. a.y) +. (a.w *. b.y)
+  and z = (a.x *. b.y) -. (a.y *. b.x) +. (b.w *. a.z) +. (b.z *. a.w)
+  and w = (a.w *. b.w) -. (a.x *. b.x) -. (a.y *. b.y) -. (a.z *. b.z) in
+  { x; y; z; w }
 
-let mul_scalar (x, y, z, w) s = x *. s, y *. s, z *. s, w *. s
-let div_scalar (x, y, z, w) s = x /. s, y /. s, z /. s, w /. s
+let mul_scalar t s = { x = t.x *. s; y = t.y *. s; z = t.z *. s; w = t.w *. s }
+let div_scalar t s = { x = t.x /. s; y = t.y /. s; z = t.z /. s; w = t.w /. s }
 let negate q = mul_scalar q (-1.)
-let norm (x, y, z, w) = Float.sqrt ((x *. x) +. (y *. y) +. (z *. z) +. (w *. w))
+let norm { x; y; z; w } = Float.sqrt ((x *. x) +. (y *. y) +. (z *. z) +. (w *. w))
 
 let normalize t =
   let n = norm t in
   if n > 0. then div_scalar t n else t
 
-let dot (x1, y1, z1, w1) (x2, y2, z2, w2) =
-  (x1 *. x2) +. (y1 *. y2) +. (z1 *. z2) +. (w1 *. w2)
-
-let conj (x, y, z, w) = -.x, -.y, -.z, w
+let dot a b = (a.x *. b.x) +. (a.y *. b.y) +. (a.z *. b.z) +. (a.w *. b.w)
+let conj t = { x = -.t.x; y = -.t.y; z = -.t.z; w = t.w }
 let distance a b = norm (sub a b)
 
 let of_rotmatrix m =
@@ -46,28 +49,28 @@ let of_rotmatrix m =
     and x = (g 2 1 -. g 1 2) /. s
     and y = (g 0 2 -. g 2 0) /. s
     and z = (g 1 0 -. g 0 1) /. s in
-    x, y, z, w
+    { x; y; z; w }
   | _ when g 0 0 > g 1 1 && g 0 0 > g 2 2 ->
     let s = Float.sqrt (1. +. g 0 0 -. g 1 1 -. g 2 2) *. 2. in
     let w = (g 2 1 -. g 1 2) /. s
     and x = 0.25 *. s
     and y = (g 0 1 +. g 1 0) /. s
     and z = (g 0 2 +. g 2 0) /. s in
-    x, y, z, w
+    { x; y; z; w }
   | _ when g 1 1 > g 2 2 ->
     let s = Float.sqrt (1. +. g 1 1 -. g 0 0 -. g 2 2) *. 2. in
     let w = (g 0 2 -. g 2 0) /. s
     and x = (g 0 1 +. g 1 0) /. s
     and y = 0.25 *. s
     and z = (g 1 2 +. g 2 1) /. s in
-    x, y, z, w
+    { x; y; z; w }
   | _ ->
     let s = Float.sqrt (1. +. g 2 2 -. g 0 0 -. g 1 1) *. 2. in
     let w = (g 1 0 -. g 0 1) /. s
     and x = (g 0 2 +. g 2 0) /. s
     and y = (g 1 2 +. g 2 1) /. s
     and z = 0.25 *. s in
-    x, y, z, w
+    { x; y; z; w }
 
 let of_euler (x_roll, y_pitch, z_yaw) =
   let open Float in
@@ -81,9 +84,9 @@ let of_euler (x_roll, y_pitch, z_yaw) =
   and x = (sr *. cp *. cy) -. (cr *. sp *. sy)
   and y = (cr *. sp *. cy) +. (sr *. cp *. sy)
   and z = (cr *. cp *. sy) -. (sr *. sp *. cy) in
-  x, y, z, w
+  { x; y; z; w }
 
-let to_rotmatrix (x, y, z, w) =
+let to_rotmatrix { x; y; z; w } =
   let s =
     let len_sqr = (x *. x) +. (y *. y) +. (z *. z) +. (w *. w) in
     if len_sqr != 0. then 2. /. len_sqr else 0.
@@ -101,11 +104,11 @@ let to_rotmatrix (x, y, z, w) =
 
 let to_euler t = RotMatrix.to_euler (to_rotmatrix t)
 let to_multmatrix ?(trans = Vec3.zero) t = MultMatrix.of_rotmatrix (to_rotmatrix t) trans
-let to_string (x, y, z, w) = Printf.sprintf "[%f, %f, %f, %f]" x y z w
-let get_x (x, _, _, _) = x
-let get_y (_, y, _, _) = y
-let get_z (_, _, z, _) = z
-let get_w (_, _, _, w) = w
+let to_string { x; y; z; w } = Printf.sprintf "[%f, %f, %f, %f]" x y z w
+let get_x t = t.x
+let get_y t = t.y
+let get_z t = t.z
+let get_w t = t.w
 
 let slerp a b =
   let a = normalize a
@@ -129,8 +132,8 @@ let slerp a b =
     | d -> compute a b d
 
 let rotate_vec3 t (x, y, z) =
-  let r = x, y, z, 0. in
-  mul (mul t r) (conj t) |> fun (x', y', z', _) -> x', y', z'
+  let r = { x; y; z; w = 0. } in
+  mul (mul t r) (conj t) |> fun { x; y; z; _ } -> x, y, z
 
 let rotate_vec3_about_pt t p v = Vec3.(rotate_vec3 t (v <+> p) <-> p)
 
@@ -149,4 +152,4 @@ let alignment v1 v2 =
   else (
     let x, y, z = Vec3.(cross v1 v2) in
     let w = Vec3.((norm v1 *. norm v2) +. dot v1 v2) in
-    normalize (x, y, z, w) )
+    normalize { x; y; z; w } )
