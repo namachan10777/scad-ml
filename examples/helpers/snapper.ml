65e0ca0c2af842bd2f2d9678ed75e7d737ede40c
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-06-26 23:04:40 -0700

    WIP adding first example page

--- examples/helpers/snapper.ml
+++ examples/helpers/snapper.ml
@@ -4,6 +4,8 @@ let () =
   let f i =
     let n = Filename.(chop_extension @@ basename Sys.argv.(i + 1)) ^ ".png" in
     let f e = Printf.printf "Failed to take snapshot %s:\n%s\n%!" n e in
-    Result.iter_error f Export.(snapshot ~colorscheme:TomorrowNight n Sys.argv.(i + 1))
+    Result.iter_error
+      f
+      Export.(snapshot ~colorscheme:TomorrowNight ~size:(1000, 1000) n Sys.argv.(i + 1))
   in
   List.init (Array.length Sys.argv - 1) (Thread.create f) |> List.iter Thread.join
